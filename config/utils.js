afcede547d10b22509896b413e69e17b62bb3a7b
Nguyen Phuc Phi <phiphuc1994@gmail.com>
2020-04-09 19:40:05 +0700

    update mail to

--- config/utils.js
+++ config/utils.js
@@ -2,5 +2,5 @@ module.exports = {
     username: "nguyenphucanh94@gmail.com",
     password: "nguyenphucphuong",
     key: "7ce7ef45-3e8c-48d8-a0fe-1dae9ed40646",
-    to:"vietptit103@gmail.com"
+    to:"info@alphawaytech.com "
 }
